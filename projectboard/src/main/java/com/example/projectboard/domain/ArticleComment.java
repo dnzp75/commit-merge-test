0cf783f16b3aacdc5260f07bf7d59d43cc24abe7
이건희 <dnzp74@g.eulji.ac.kr>
2023-12-06 08:49:19 +0900

    #15 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로
    엔티티 클래스에 들어가는 요소이고,
    도메인과 직접 연관이 없는 요소이므로
    추출이 가능하다.
    
    그 외에 idea 파일에 있는 것들도 같이 커밋 후 푸시

--- projectboard/src/main/java/com/example/projectboard/domain/ArticleComment.java
+++ projectboard/src/main/java/com/example/projectboard/domain/ArticleComment.java
@@ -1,6 +1,5 @@
 package com.example.projectboard.domain;
 
-import jakarta.persistence.GeneratedValue;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,9 +8,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.*;
-
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -22,43 +19,34 @@ import java.util.Objects;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@EntityListeners(AuditingEntityListener.class)
+
 @Entity
-public class ArticleComment {
+    public class ArticleComment extends AuditingFields {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
+        @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
-    @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
-    @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy; // 수정자
+        protected ArticleComment() {}
 
-    protected ArticleComment() {}
-
-    private ArticleComment(Article article, String content) {
-        this.article = article;
-        this.content = content;
+        private ArticleComment(Article article, String content) {
+            this.article = article;
+            this.content = content;
+        }
+        public static ArticleComment of(Article article, String content) {
+            return new ArticleComment(article, content);
+        }
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof ArticleComment that)) return false;
+            return id != null && id.equals(that.id);
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
     }
-
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof ArticleComment that)) return false;
-        return id != null && id.equals(that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-}
